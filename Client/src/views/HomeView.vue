7df987ac1fb05393612022de39e412bb62fe7211
Edanriell <Edanriell@gmail.com>
2025-10-02 10:09:41 +0300

    feature(initial-config): implemented

--- Client/src/views/HomeView.vue
+++ Client/src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+<script lang="ts" setup>
+	import TheWelcome from "../components/TheWelcome.vue";
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+	<main>
+		<TheWelcome />
+	</main>
 </template>
