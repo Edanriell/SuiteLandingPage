a535f3cacedd1b615e9031dd7c8ca40de17dd951
Edanriell <Edanriell@gmail.com>
2025-10-16 23:17:12 +0300

    feature(atomic-design-adoption): created base for architecture

---
+++ Client/app/pages/index.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+
+import Home from "~/components/templates/pages/home/home.vue";
+
+definePageMeta({
+	layout: "default"
+})
+</script>
+
+<template>
+	<Home />
+</template>
