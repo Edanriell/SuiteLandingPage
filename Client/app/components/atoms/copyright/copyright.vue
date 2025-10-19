ba03b019572b5a3dc5f720a4ac73be9b6e58a7e9
Edanriell <Edanriell@gmail.com>
2025-10-19 22:35:41 +0300

    feature(copyright): created atom

---
+++ Client/app/components/atoms/copyright/copyright.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+	<small
+		class="font-[Epilogue] font-normal text-[15rem] leading-[160%] text-[var(--colors-neutral-500)]"
+	>
+		Copyright - Suite
+	</small>
+</template>
--- Client/app/components/organisms/footer/footer.vue
+++ Client/app/components/organisms/footer/footer.vue
@@ -1,11 +1,13 @@
 <script lang="ts" setup>
-import { Logotype } from "@atoms";
+	import { Copyright, Logotype } from "@atoms";
 </script>
 
 <template>
-	<footer>
+	<footer
+		class="ml-[32rem] mr-[32rem] pt-[56.2rem] pb-[40rem] flex flex-col items-center justify-center gap-y-[32rem] tablet:flex-row tablet:justify-between tablet:pt-[79.2rem] desktop:ml-[166.79rem] desktop:mr-[166.79rem] desktop:pt-[104.2rem] desktop:pb-[80rem]"
+	>
 		<Logotype />
-		<small>Copyright - Suite</small>
+		<Copyright />
 		<ul>
 			<li>
 				<span class="sr-only">Link to our Facebook page</span>
