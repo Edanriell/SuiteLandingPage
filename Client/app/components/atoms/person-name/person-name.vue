55d1ec619a072adead2d73b23bf65764b859a4d6
Edanriell <Edanriell@gmail.com>
2025-11-27 21:00:15 +0200

    feature(person-name): implemnted

--- Client/app/components/atoms/person-name/person-name.vue
+++ Client/app/components/atoms/person-name/person-name.vue
@@ -1,3 +1,9 @@
 <script lang="ts" setup></script>
 
-<template></template>
+<template>
+	<span
+		class="font-[Epilogue] font-bold text-[18rem] leading-[160%] tracking-[-0.01em] uppercase text-[var(--colors-neutral-0)]"
+	>
+		<slot />
+	</span>
+</template>
